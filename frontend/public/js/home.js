8ce419e0a9299bde8291d627ebdb8accc1486f32
松添大起 <matsutaiyaki1013@yahoo.co.jp>
2025-12-02 20:10:11 +0900

    フォームの要素をserver.jsに送信し、resultを表示するAPIを作成

--- frontend/public/js/home.js
+++ frontend/public/js/home.js
@@ -3,30 +3,29 @@ document.addEventListener("DOMContentLoaded", () => {
     const nameInput = document.getElementById('nameInput');
     const weightInput = document.getElementById('weightInput');
     const repsInput = document.getElementById('repsInput');
+    const setsInput = document.getElementById('setsInput');
 
     // ワークアウト追加フォームイベントを監視する
-    addWorkout.addEventListener('click', async(e) => {
+    addWorkout.addEventListener('submit', async(e) => {
         e.preventDefault();
         const name = nameInput.value;
         const weight = weightInput.value;
         const reps = repsInput.value;
-
-        if (name === "") {
-            alert("種目名を入力してください");
-        }else {
-            data = {
+        const sets = setsInput.value;
+        data = {
                 name: name,
                 weight: weight,
-                reps: reps
+                reps: reps,
+                sets: sets
             };
-            const response = await fetch("/api/add/workout", {
-            method: "POST",
-            headers: { "Content-type": "application/json" },
-            body: JSON.stringify(data),
-            });
-            const result = await response.json();
-            alert("追加が完了しました!");
-        }
+        console.log(data)
+        const response = await fetch("/api/add/workouts", {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify(data),
+        });
+        const result = await response.json();
+        alert(`${result.message}`);
     });
 
     // ワークアウト一覧を表示する
